9cc6dcb239eb43d0aaab210f55bd3ac5930f756e
xtinktstudios <studios.xtinkt@gmail.com>
2023-05-11 15:09:31 -0500

    hehe

--- Door.java
+++ Door.java
@@ -4,9 +4,10 @@ import java.util.*;
 import java.lang.Math;
 
 public class Door {
-	private ArrayList<ArrayList<String>> deck;
-	private Stack<ArrayList<String>> discard;
+	private ArrayList<ArrayList<String>> deck,cdeck;
+	private Stack<ArrayList<String>> discard,cdiscard;
 	private Path path = Paths.get("src\\door");
+	private Path cpath = Paths.get("src\\curse");
 
 	public Door() throws Exception{
 		deck = new ArrayList<ArrayList<String>>();
@@ -20,6 +21,23 @@ public class Door {
 				info.add(scan.nextLine().substring(2));
 			}
 			deck.add(info);
+		}
+	}
+	
+	
+	public void curse() throws Exception{
+		cdeck = new ArrayList<ArrayList<String>>();
+		cdiscard = new Stack<ArrayList<String>>();
+		this.path = Paths.get("src\\curse");
+
+		for(int i = 1; i < getFilesCount(cpath); i++){
+			Scanner scan = new Scanner(new File("src\\curse\\curse " + i + ".dat"));
+			// Scanner scan = new Scanner(new File(path + "\\curse " + i + ".dat")); // This would also work btw since yous assigned a variable for the first part
+			ArrayList<String> info = new ArrayList<String>();
+			while(scan.hasNextLine()) {
+				info.add(scan.nextLine().substring(2));
+			}
+			cdeck.add(info);
 		}
 	}
 
